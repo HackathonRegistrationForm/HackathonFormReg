7807397c92bafe25f399044d21152942ed034017
Umavengalaraju <umavengalaraju20@gmail.com>
2023-01-29 23:34:27 +0530

    second

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { subscribeOn } from 'rxjs';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -11,52 +13,85 @@ export class FormComponent {
 
 
   hideform=false;
-    fname: any;
-    lname: any;
-    email: any;
-    interestedtechnology: any;
-    projecttopic: any;
-    gender: any;
+   submitted:any
     Obj: any;
+  hacathon: any;
+
   
   ngOnInit(): void {
+    this.hacathon = new FormGroup(
+      {
+        fname: new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+        lname : new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+        projecttopic : new FormControl('',[Validators.required]),
+        email : new FormControl('',[Validators.required,Validators.email]),
+        interestedtechnology:new FormControl('',[Validators.required]),
+        gender : new FormControl('',[Validators.required]),
+      sid:new FormControl(0)
+      });
   }
-  submit(){
-  console.log(this.fname)
-  console.log(this.lname)
-  console.log(this.email)
-  console.log(this.projecttopic)
-  console.log(this.interestedtechnology)
-  console.log(this.gender)
-{
-   const Obj={
-     fname: this.fname,
-     lname:this.lname,
-     email:this.email,
-     projecttopic:this.projecttopic,
-     interestedtechnology:this.interestedtechnology,
-     gender:this.gender
-   }
-  var requestOptions = {
-    method: 'POST',
-    body:JSON.stringify(Obj)
-  };
-  console.log(requestOptions);
+
+    get f() { return this.hacathon.controls; }
+    
+    onSubmit() {
+     
+      if (this.hacathon.value.fname==''||this.hacathon.value.lname==''||this.hacathon.value.email==''||this.hacathon.value.projecttopic==''||this.hacathon.value.interestedtechnology==''||this.hacathon.value.gender=='') {
+        Swal.fire(  
+          'Cancelled',  
+          'You Must  Enter All fields !',           //give for condition to take all properties take empty values
+          'error'                                  //then take one alert message like not save all data
+        )  
+    }else
+    {
+      this.hacathon.value.sid = this.Obj;  
+      Swal.fire('Added Successfully!', '', 'success').then(() => {
+      }); 
+      var requestOptions = {
+        method: 'POST',
+        body:JSON.stringify(this.hacathon.value)
+      };
+      console.log(requestOptions);  
   fetch("http://localhost:7000/employee/addemployee",{
     method:'POST',
     headers:{
       "Access-Control-Allow-Origin":"*",
       "Content-Type":'application/json'
     },
-    body:JSON.stringify(Obj)
+    body:JSON.stringify(this.hacathon.value)
   })  .then(response => response.json())
   .then(result =>
     console.log(result))
   .catch(error => console.log('error',error));
     }
-
-  window.location.href="./show"
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.hacathon.value, null, 4));
   }
+  onsubmit(){
 
+  }
+  get fname()
+  {
+   return this.hacathon.get('fname');
+  }
+  get lname()
+  {
+   return this.hacathon.get('lname') ;
+  }
+  get projecttopic()
+  {
+   return this.hacathon.get('projecttopic') ;
+  }
+  get interestedtechnology()
+  {
+   return this.hacathon.get('interestedtechnology') ;
+  }
+  get email()
+  {
+   return this.hacathon.get('email') ;
+  }
+  get gender()
+  {
+   return this.hacathon.get('gender') ;
+  }
+ 
 }
 
